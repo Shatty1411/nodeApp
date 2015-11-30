afa29e4c4514a6e4656cc93a258e850515d39316
1400004 <love.1.alone@hotmail.com>
2015-11-30 14:16:21 +0000

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Created by 1400004 on 30/11/2015.
  */
 
+
 var http = require('http')
 var port = process.env.PORT || 1337;
 
@@ -9,4 +10,3 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
 }).listen(port);
-
